1a681ee2f14ced77cb9c0ea0d5b52e41c0fe9716
Annisa Putri <annisaputriwahyuni46@gmail.com>
2024-09-16 20:44:07 +0700

    Add home route

--- app.py
+++ app.py
@@ -2,5 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask REST API!"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
