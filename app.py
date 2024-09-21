4b5aa3f8ca35ff2b7cae667e1ceb9905ddf93f08
Annisa Putri <annisaputriwahyuni46@gmail.com>
2024-09-21 20:53:08 +0700

    change data from item to product

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-items = [
-    {"id": 1, "name": "Item 1", "description": "First item"},
-    {"id": 2, "name": "Item 2", "description": "Second item"},
+products = [
+    {"id": 1, "name": "product 1", "description": "First product", "price": 1000},
+    {"id": 2, "name": "product 2", "description": "Second product", "price": 2000},
 ]
 
 
@@ -13,45 +13,54 @@ def home():
     return "Welcome to the Flask REST API!"
 
 
-@app.route("/api/items", methods=["GET"])
-def get_items():
-    return {"items": items}
+@app.route("/products", methods=["GET"])
+def get_products():
+    return {"data": products}
 
 
-@app.route("/api/items", methods=["POST"])
-def create_item():
-    new_item = request.get_json()  # Get the new item from the request body
-    items.append(new_item)
-    return {"item": new_item}, 201  # Return 201 status to indicate creation
+@app.route("/products", methods=["POST"])
+def create_product():
+    new_product = request.get_json()  # Get the new item from the request body
+    products.append(new_product)
+    return {"data": new_product}, 201  # Return 201 status to indicate creation
 
 
-@app.route("/api/items/<int:item_id>", methods=["GET"])
-def get_item(item_id):
-    item = next((item for item in items if item["id"] == item_id), None)
-    if item:
-        return {"item": item}
+@app.route("/products/<int:product_id>", methods=["GET"])
+def get_product_by_id(product_id):
+    product_by_id = next((p for p in products if p["id"] == product_id), None)
+    if product_by_id:
+        return {"data": product_by_id}
     else:
-        return {"message": "Item not found"}, 404
+        return {
+            "message": "Product not found"
+        }, 404  # Return 400 to indicate no item found
 
 
-@app.route("/api/items/<int:item_id>", methods=["PUT"])
-def update_item(item_id):
-    updated_item = request.get_json()
-    item = next((item for item in items if item["id"] == item_id), None)
+@app.route("/products/<int:product_id>", methods=["PUT"])
+def update_product(product_id):
+    updated_product = request.get_json()
+    product_to_update = next((p for p in products if p["id"] == product_id), None)
 
-    if item:
+    if product_to_update:
         # Perbarui atribut item yang ada
-        item["name"] = updated_item.get("name", item["name"])
-        item["description"] = updated_item.get("description", item["description"])
-    return {"message": f"Item {item_id} updated", "item": item}
+        product_to_update["name"] = updated_product.get(
+            "name", product_to_update["name"]
+        )
+        product_to_update["description"] = updated_product.get(
+            "description", product_to_update["description"]
+        )
+        product_to_update["price"] = updated_product.get(
+            "price", product_to_update["price"]
+        )
+    return {"message": f"Product {product_id} updated", "data": product_to_update}
 
 
-@app.route("/api/items/<int:item_id>", methods=["DELETE"])
-def delete_item(item_id):
-    item = next((item for item in items if item["id"] == item_id), None)
-    if item:
-        items.remove(item)
-    return {"message": f"Item {item_id} deleted"}
+@app.route("/products/<int:product_id>", methods=["DELETE"])
+def delete_product(product_id):
+    product_to_delete = next((p for p in products if p["id"] == product_id), None)
+    if product_to_delete:
+        products.remove(product_to_delete)
+    return {"message": f"Product {product_id} deleted"}
 
 
 if __name__ == "__main__":
